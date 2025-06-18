ef01b69b4857a80537f87914defe9fd582ca0894
Arturo Freites <arturo@qrsolutions.com.ar>
2025-06-18 04:47:56 -0300

    Se quitan params del componente carrousel

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -3,12 +3,9 @@ import HomeCarrousel from "@/components/Home/HomeCarrousel";
 
 export default function Home() {
 
-  const images = ["/img/bg.webp", "/img/bg1.webp"]
-  const imagesMobile = ["/img/bg-mobile.webp", "/img/bg-mobile1.webp"]
-
   return (
     <section className="flex flex-col ">
-      <HomeCarrousel imagesMobile={imagesMobile} images={images} interval={5000} />
+      <HomeCarrousel/>
       <Content/>
     </section>
   );
--- components/Home/HomeCarrousel.tsx
+++ components/Home/HomeCarrousel.tsx
@@ -3,7 +3,6 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import Image from 'next/image'
 
 import { supabaseBrowser } from '@/lib/superbase'
 
