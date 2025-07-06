d619116ea12931ce805401e1022d249693c770ac
Arturo Freites <arturo@qrsolutions.com.ar>
2025-07-06 12:22:43 -0300

    Se añade estadisticas de vercel

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,10 +1,12 @@
 import Content from "@/components/Home/Content";
 import HomeCarrousel from "@/components/Home/HomeCarrousel";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
 
   return (
     <section className="flex flex-col ">
+      <SpeedInsights/>
       <HomeCarrousel/>
       <Content/>
     </section>
